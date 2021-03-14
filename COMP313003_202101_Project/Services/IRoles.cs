13a713d338e12e77c973c09c5f71a61885a6dd80
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 08:07:51 -0400

    Feat: users/authorization: Create interface for roles

---
+++ COMP313003_202101_Project/Services/IRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP313003_202101_Project.Services
+{
+    public interface IRoles
+    {
+        Task GenerateRolesFromPagesAsync();
+
+        Task AddToRoles(string applicationUserId);
+    }
+}
