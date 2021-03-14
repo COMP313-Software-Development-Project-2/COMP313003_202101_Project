54cd9c8335c4aed2dc0c820653d783fd32b0ea55
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 07:48:17 -0400

    Feat: users: Create controllers for views related to users

---
+++ COMP313003_202101_Project/Areas/Identity/Controllers/CustomerTypeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using COMP313003_202101_Project.Pages;
+
+namespace COMP313003_202101_Project.Areas.Identity.Controllers
+{
+    [Authorize(Roles = MainMenu.CustomerType.RoleName)]
+    public class CustomerTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP313003_202101_Project/Areas/Identity/Controllers/UserRoleController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP313003_202101_Project.Areas.Identity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using COMP313003_202101_Project.Pages;
+
+namespace COMP313003_202101_Project.Areas.Identity.Controllers
+{
+    
+    public class UserRoleController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserRoleController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [Authorize(Roles = MainMenu.User.RoleName)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = MainMenu.ChangePassword.RoleName)]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = MainMenu.Role.RoleName)]
+        public IActionResult Role()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = MainMenu.ChangeRole.RoleName)]
+        public IActionResult ChangeRole()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> UserProfile()
+        {
+            User user = await _userManager.GetUserAsync(User);
+            return View(user);
+        }
+    }
+}
