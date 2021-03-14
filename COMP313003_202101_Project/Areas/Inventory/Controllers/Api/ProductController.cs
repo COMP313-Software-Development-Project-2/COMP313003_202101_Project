b6c16cac53a3529323350c602aade8915585e60e
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 11:25:58 -0400

    Feat: inventory: Add CRUD controllers for inventory related models

---
+++ COMP313003_202101_Project/Areas/Inventory/Controllers/Api/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP313003_202101_Project.Data;
+using COMP313003_202101_Project.Areas.Inventory.Models;
+using COMP313003_202101_Project.ViewModels.SyncfusionViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace COMP313003_202101_Project.Areas.Inventory.Controllers.Api
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/Item")]
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Item
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            List<Item> Items = await _context.Item.ToListAsync();
+            int Count = Items.Count();
+            return Ok(new { Items, Count });
+        }
+
+
+
+        [HttpPost("[action]")]
+        public IActionResult Insert([FromBody]CrudViewModel<Item> payload)
+        {
+            Item product = payload.value;
+            _context.Item.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Update([FromBody]CrudViewModel<Item> payload)
+        {
+            Item product = payload.value;
+            _context.Item.Update(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Remove([FromBody]CrudViewModel<Item> payload)
+        {
+            Item product = _context.Item
+                .Where(x => x.ItemId == (int)payload.key)
+                .FirstOrDefault();
+            _context.Item.Remove(product);
+            _context.SaveChanges();
+            return Ok(product);
+
+        }
+    }
+}
---
+++ COMP313003_202101_Project/Areas/Inventory/Controllers/Api/ProductTypeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP313003_202101_Project.Data;
+using COMP313003_202101_Project.Areas.Inventory.Models;
+using COMP313003_202101_Project.ViewModels.SyncfusionViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace COMP313003_202101_Project.Areas.Inventory.Controllers.Api
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/ItemType")]
+    public class ItemTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ItemType
+        [HttpGet]
+        public async Task<IActionResult> GetProductType()
+        {
+            List<ItemType> Items = await _context.ItemType.ToListAsync();
+            int Count = Items.Count();
+            return Ok(new { Items, Count });
+        }
+
+
+
+        [HttpPost("[action]")]
+        public IActionResult Insert([FromBody]CrudViewModel<ItemType> payload)
+        {
+            ItemType productType = payload.value;
+            _context.ItemType.Add(productType);
+            _context.SaveChanges();
+            return Ok(productType);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Update([FromBody]CrudViewModel<ItemType> payload)
+        {
+            ItemType productType = payload.value;
+            _context.ItemType.Update(productType);
+            _context.SaveChanges();
+            return Ok(productType);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Remove([FromBody]CrudViewModel<ItemType> payload)
+        {
+            ItemType productType = _context.ItemType
+                .Where(x => x.ItemTypeId == (int)payload.key)
+                .FirstOrDefault();
+            _context.ItemType.Remove(productType);
+            _context.SaveChanges();
+            return Ok(productType);
+
+        }
+    }
+}
