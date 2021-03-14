231d89d13fbcb52cdabf48f57b88a6777dcb491b
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 08:07:10 -0400

    Feat: users/email-confirmation: Create classes for email services

---
+++ COMP313003_202101_Project/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP313003_202101_Project.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
