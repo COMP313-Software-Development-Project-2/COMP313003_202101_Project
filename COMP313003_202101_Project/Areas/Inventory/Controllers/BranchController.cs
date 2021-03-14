5d39a9f6c9ee68f241369c496131a750c93a221d
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 11:24:19 -0400

    Feat: inventory: Create controllers for views related to inventory

---
+++ COMP313003_202101_Project/Areas/Inventory/Controllers/BranchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP313003_202101_Project.Areas.Inventory.Controllers
+{
+    [Authorize(Roles = Pages.MainMenu.Branch.RoleName)]
+    public class BranchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
