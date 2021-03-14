7bc885876247acf2851d935b894d2cbee8a5c014
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 11:31:24 -0400

    Feat: order-fulfillment: Add models for order fullfillment

---
+++ COMP313003_202101_Project/Areas/OrderFulfillment/Models/DeliveryOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP313003_202101_Project.Areas.OrderFulfillment.Models
+{
+    public class DeliveryOrder
+    {
+        [Required]
+        [Display(Name = "Delivery Order Id")]
+        public int DeliveryOrderId { get; set; }
+        [Display(Name = "Shipment Number")]
+        public string DeliveryOrderName { get; set; }
+        [Display(Name = "Delivery Date")]
+        public DateTimeOffset DeliveryDate { get; set; }
+        [Display(Name = "Delivery Type Id")]
+        public int DeliveryTypeId { get; set; }
+        [Display(Name = "Status Id")]
+        public int Status { get; set; }
+        [Display(Name = "Warehouse Id")]
+        public int WarehouseId { get; set; }
+        [Display(Name = "Purchase Order Id")]
+        public int PurchaseOrderId { get; set; }
+        [Display(Name = "Sales Order Id")]
+        public int SalesOrderId { get; set; }
+        [Display(Name = "Shipment Id")]
+        public int ShipmentId { get; set; }
+        [Display(Name = "Full Delivery")]
+        public bool IsFullDelivery { get; set; } = true;
+    }
+}
---
+++ COMP313003_202101_Project/Areas/OrderFulfillment/Models/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP313003_202101_Project.Areas.OrderFulfillment.Models
+{
+    public enum Status
+    {
+        Requested = 0,
+        Accepted = 1,
+        Denied = 2,
+        Processing = 3,
+        Shipped = 4,
+        Delivered = 5,
+        OnHold = 6,
+        Cancelled = 7,
+        Returned = 8
+    }
+}
