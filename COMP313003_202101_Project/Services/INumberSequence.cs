c6be18a21b8221063fca036aa527504cb286ee26
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-14 08:45:24 -0400

    Feat: general: Add number sequence class

---
+++ COMP313003_202101_Project/Services/INumberSequence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP313003_202101_Project.Services
+{
+    public interface INumberSequence
+    {
+        string GetNumberSequence(string module);
+    }
+}
