a985913a5f33ea4d30040f3b0d2418323a3e2223
Koolait Roa <kroa1@my.centennialcollege.ca>
2021-03-15 03:56:19 -0400

    Feature: database: Add database initializer

---
+++ COMP313003_202101_Project/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using COMP313003_202101_Project.Areas.Identity.Models;
+using COMP313003_202101_Project.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace COMP313003_202101_Project.Data
+{
+    public static class DbInitializer
+    {
+        public static async Task Initialize(ApplicationDbContext context,
+           IFunctional functional)
+        {
+            context.Database.EnsureCreated();
+            if (context.User.Any())
+            {
+                return; //if user is not empty, DB has been seed
+            }
+
+            await functional.Initialize();
+
+            await functional.InitAppData();
+        }
+    }
+}
